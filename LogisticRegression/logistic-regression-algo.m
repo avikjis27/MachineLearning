9c7ceafa8a1e84cfe2783b77dc0e7a613f44a662
Avik Chakraborty <avik.chakraborty@lexmark.com>
2017-09-14 19:09:23 +0530

    plot added

--- LogisticRegression/logistic-regression-algo.m
+++ LogisticRegression/logistic-regression-algo.m
@@ -8,11 +8,24 @@ display_matrix('size of X after appending one', X);# 30 * 4
 
 ACTUAL_OUTPUT= [data_file(:, end)];
 
+POSITIVE_DATA_INDX = (data_file(: , end)==1);
+NEGETIVE_DATA_INDX = (data_file(: , end)==0);
+
+POSITIVE_DATA = data_file(POSITIVE_DATA_INDX,:);
+NEGETIVE_DATA = data_file(NEGETIVE_DATA_INDX,:);
+
+POSITIVE_DATA = POSITIVE_DATA(:,1:end-1)
+NEGETIVE_DATA = NEGETIVE_DATA(:,1:end-1)
+plot(POSITIVE_DATA);
+plot(NEGETIVE_DATA);
+
+
 THETA = zeros(1,columns(X));
 alpha = 0.00001;
 steps = 300;
 printf("Initial THETA is :"), disp(THETA);
 printf("alpha is :%f \n", alpha);
+
 costs = [];
 for i = 1:steps
 	fprintf("Iteration ---------------%d\n",i);
